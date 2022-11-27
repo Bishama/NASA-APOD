81b84a1e3c4671c78042e2554b6ee9ee1c7d0aba
Bishama Irfan <bishamairfann10@gmail.com>
2022-11-27 15:14:21 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+
+document.querySelector('button').addEventListener('click', getData)
+
+function getData(){
+  const date = document.querySelector('input').value
+  fetch(`https://api.nasa.gov/planetary/apod?api_key=ciOPRZIsf9D7FYlGIfgXth7j7CttJalGkDrRvlgu&date=${date}`)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data)
+      document.querySelector('h2').innerText = data.title;
+      document.querySelector('img').src = data.url;
+      document.querySelector('h3').innerText = data.explanation;
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
